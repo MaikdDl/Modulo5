96aaf19676ebd3b0f6596f389bd55323fefccfe5
MaikdDl <maikdl@gmail.com>
2019-03-28 20:00:31 +0100

    Añadidas dashboard y detalles de los heroes

--- tour-of-heroes/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -9,21 +9,17 @@ import { HeroService } from "../hero.service";
 })
 export class HeroesComponent implements OnInit {
 
-  selectedHero: Hero;
   heroes: Hero[];
 
-  getHeroes(): void {
-    this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
-  }
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
 }
